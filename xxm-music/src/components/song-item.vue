3f8ffae549b53075f42cb13e48b80eb807ba91c1
guochunfu <guochunfu@jezoe.com>
2017-12-14 18:25:05 +0800

    获取歌单

---
+++ xxm-music/src/components/song-item.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="song-item">
+    <img :src="bgUrl" alt="">
+    <span class="playnum"><i class="icon icon-music"></i>{{playnum}}万</span>
+    <p class="disc">{{disc}}</p>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      bgUrl: {
+        type: String,
+        default: ''
+      },
+      playnum: {
+        type: String,
+        default: ''
+      },
+      disc: {
+        type: String,
+        default: ''
+      }
+    },
+    data() {
+      return {}
+    }
+  }
+
+</script>
+<style lang="less" scoped>
+  @import '~common/base.less';
+  .song-item {
+    display: inline-block;
+    position: relative;
+    overflow: hidden;
+    width: 2.44rem;
+   margin-bottom: 0.2rem;
+    img {
+      height: 2.5rem;
+    }
+    .playnum{
+        color: #fff;
+        font-size: 0.26rem;
+        position: absolute;
+        right: 0.2rem;
+        top: 0.1rem;
+        .icon{
+            padding-right: 0.1rem;
+        }
+    }
+    .disc {
+      line-height: 0.36rem;
+      padding:0.1rem;
+      color: #333;
+    }
+  }
+
+</style>
---
+++ xxm-music/src/views/find-music/recommoned.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="page" v-loading="$store.state.isLoading">
+    <div class="banner">
+      <el-carousel height="2rem" indicator-position="none">
+        <el-carousel-item v-for="item in banner" :key="item.targetId">
+          <img :src="item.pic" alt="" class="bannerimg">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="recommend-songlist">
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+      <song-item :disc="disc" :bgUrl="url" :playnum="num"></song-item>
+    </div>
+  </div>
+</template>
+<script>
+  import {
+    Banner
+  } from '@/api/api.js'
+  import songItem from '@/components/song-item.vue'
+  export default {
+    name: 'find-music',
+    data() {
+      return {
+        activeName: 'first',
+        banner: [],
+        num: '1823',
+        url: 'http://p1.music.126.net/2zSNIqTcpHL2jIvU6hG0EA==/109951162868128395.jpg',
+        disc: '第覅文化覅无法和我i凤凰网i方式发和上次沉思'
+      }
+    },
+    mounted() {
+      Banner()
+        .then(res => {
+          this.banner = res.banners
+        })
+    },
+
+    components: {
+      songItem
+    },
+    methods: {}
+  }
+
+</script>
+<style scoped lang="less">
+  .page {
+    position: relative;
+    height: 100%;
+    .banner {
+      height: 2rem;
+      margin-bottom: 0.2rem;
+      position: relative;
+      .bannerimg {
+        width: 100%;
+        height: 2rem;
+      }
+    }
+    .recommend-songlist {
+      position: relative;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+  }
+
+</style>
